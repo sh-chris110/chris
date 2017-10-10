3f2edfdcdc68650fae46215ed91044a85879f87f
chris <13918682106@163.com>
2017-10-10 18:06:23 +0800

    add python openGL files
    Signed-off-by: chris <13918682106@163.com>

---
+++ python/cube.py
@@ -0,0 +1,161 @@
+import sys
+from OpenGL.GL import *
+from OpenGL.GLUT import *
+from OpenGL.GLU import *
+from PIL import Image
+
+class MyPyOpenGLTest:
+    def __init__(self,
+                 width=640,
+                 height=480,
+                 title='MyPyOpenGLTest'.encode()):
+        glutInit(sys.argv)
+        glutInitDisplayMode(GLUT_RGBA | GLUT_DOUBLE | GLUT_DEPTH)
+        glutInitWindowSize(width, height)
+        self.window = glutCreateWindow(title)
+        glutDisplayFunc(self.Draw)
+        glutIdleFunc(self.Draw)
+        self.InitGL(width, height)
+        self.x = 0.0
+        self.y = 0.0
+        self.z = 0.0
+
+    def Draw(self):
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
+        glLoadIdentity()
+        glTranslate(0.0, 0.0, -5.0)
+        glRotatef(self.x, 1.0, 0.0, 0.0)
+        glRotatef(self.y, 0.0, 1.0, 0.0)
+        glRotatef(self.z, 0.0, 0.0, 1.0)
+
+        glBindTexture(GL_TEXTURE_2D, 0)
+        glBegin(GL_QUADS)        
+        glTexCoord2f(0.0, 0.0)
+        glVertex3f(-1.0, -1.0, 1.0)
+        glTexCoord2f(1.0, 0.0)
+        glVertex3f(1.0, -1.0, 1.0)
+        glTexCoord2f(1.0, 1.0)
+        glVertex3f(1.0, 1.0, 1.0)
+        glTexCoord2f(0.0, 1.0)
+        glVertex3f(-1.0, 1.0, 1.0)
+        glEnd()
+
+        glBindTexture(GL_TEXTURE_2D, 1)
+        glBegin(GL_QUADS)        
+        glTexCoord2f(1.0, 0.0)
+        glVertex3f(-1.0, -1.0, -1.0)
+        glTexCoord2f(1.0, 1.0)
+        glVertex3f(-1.0, 1.0, -1.0)
+        glTexCoord2f(0.0, 1.0)
+        glVertex3f(1.0, 1.0, -1.0)
+        glTexCoord2f(0.0, 0.0)
+        glVertex3f(1.0, -1.0, -1.0)
+        glEnd()
+
+        glBindTexture(GL_TEXTURE_2D, 2)
+        glBegin(GL_QUADS)
+        glTexCoord2f(0.0, 1.0)
+        glVertex3f(-1.0, 1.0, -1.0)
+        glTexCoord2f(0.0, 0.0)
+        glVertex3f(-1.0, 1.0, 1.0)
+        glTexCoord2f(1.0, 0.0)
+        glVertex3f(1.0, 1.0, 1.0)
+        glTexCoord2f(1.0, 1.0)
+        glVertex3f(1.0, 1.0, -1.0)
+        glEnd()
+
+        glBindTexture(GL_TEXTURE_2D, 3)
+        glBegin(GL_QUADS)        
+        glTexCoord2f(1.0, 1.0)
+        glVertex3f(-1.0, -1.0, -1.0)
+        glTexCoord2f(0.0, 1.0)
+        glVertex3f(1.0, -1.0, -1.0)
+        glTexCoord2f(0.0, 0.0)
+        glVertex3f(1.0, -1.0, 1.0)
+        glTexCoord2f(1.0, 0.0)
+        glVertex3f(-1.0, -1.0, 1.0)
+        glEnd()
+
+        glBindTexture(GL_TEXTURE_2D, 4)
+        glBegin(GL_QUADS)        
+        glTexCoord2f(1.0, 0.0)
+        glVertex3f(1.0, -1.0, -1.0)
+        glTexCoord2f(1.0, 1.0)
+        glVertex3f(1.0, 1.0, -1.0)
+        glTexCoord2f(0.0, 1.0)
+        glVertex3f(1.0, 1.0, 1.0)
+        glTexCoord2f(0.0, 0.0)
+        glVertex3f(1.0, -1.0, 1.0)
+        glEnd()
+
+        glBindTexture(GL_TEXTURE_2D, 5)
+        glBegin(GL_QUADS)
+        glTexCoord2f(0.0, 0.0)
+        glVertex3f(-1.0, -1.0, -1.0)
+        glTexCoord2f(1.0, 0.0)
+        glVertex3f(-1.0, -1.0, 1.0)
+        glTexCoord2f(1.0, 1.0)
+        glVertex3f(-1.0, 1.0, 1.0)
+        glTexCoord2f(0.0, 1.0)
+        glVertex3f(-1.0, 1.0, -1.0)
+        glEnd()
+
+        glutSwapBuffers()
+        self.x += 0.02
+        self.y += 0.03
+        self.z += 0.01
+
+    def LoadTexture(self):
+        imgFiles = [str(i)+'.jpeg' for i in range(1,7)]
+        for i in range(6):
+            img = Image.open(imgFiles[i])
+            width, height = img.size
+            img = img.tobytes('raw', 'RGBX', 0, -1)
+            
+            glGenTextures(2)
+            glBindTexture(GL_TEXTURE_2D, i)
+            glTexImage2D(GL_TEXTURE_2D, 0, 4,
+                         width, height, 0, GL_RGBA,
+                         GL_UNSIGNED_BYTE,img)
+            glTexParameterf(GL_TEXTURE_2D,
+                            GL_TEXTURE_WRAP_S, GL_CLAMP)
+            glTexParameterf(GL_TEXTURE_2D,
+                            GL_TEXTURE_WRAP_T, GL_CLAMP)
+            glTexParameterf(GL_TEXTURE_2D,
+                            GL_TEXTURE_WRAP_S, GL_REPEAT)
+            glTexParameterf(GL_TEXTURE_2D,
+                            GL_TEXTURE_WRAP_T, GL_REPEAT)
+            glTexParameterf(GL_TEXTURE_2D,
+                            GL_TEXTURE_MAG_FILTER, GL_NEAREST)
+            glTexParameterf(GL_TEXTURE_2D,
+                            GL_TEXTURE_MIN_FILTER, GL_NEAREST)
+            glTexEnvf(GL_TEXTURE_ENV,
+                      GL_TEXTURE_ENV_MODE, GL_DECAL)
+        
+        
+    def InitGL(self, width, height):
+        self.LoadTexture()
+        glEnable(GL_TEXTURE_2D)
+        glClearColor(1.0, 1.0, 1.0, 0.0)
+        glClearDepth(1.0)
+        glDepthFunc(GL_LESS)
+        glShadeModel(GL_SMOOTH)
+        glEnable(GL_CULL_FACE)
+        glCullFace(GL_BACK)
+        glEnable(GL_POINT_SMOOTH)
+        glEnable(GL_LINE_SMOOTH)
+        glEnable(GL_POLYGON_SMOOTH)
+        glMatrixMode(GL_PROJECTION)
+        glHint(GL_POINT_SMOOTH_HINT,GL_NICEST)
+        glHint(GL_LINE_SMOOTH_HINT,GL_NICEST)
+        glHint(GL_POLYGON_SMOOTH_HINT,GL_FASTEST)
+        glLoadIdentity()
+        gluPerspective(45.0, float(width)/float(height), 0.1, 100.0)
+        glMatrixMode(GL_MODELVIEW)
+
+    def MainLoop(self):
+        glutMainLoop()
+
+if __name__ == '__main__':
+    w = MyPyOpenGLTest()
+    w.MainLoop()
